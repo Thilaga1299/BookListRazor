72f491725742bfb3f7f329c5f62415018c9401c6
Deepika <deepika.murugesan@mnscorp.net>
2021-06-28 13:12:30 +0530

    Database added

---
+++ Model/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using BookListRazor.Model;
+using Microsoft.EntityFrameworkCore;
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Book { get; set; }
+    }
+}
---
+++ Model/Book.cs
@@ -0,0 +1,9 @@
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
